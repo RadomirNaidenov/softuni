1886fa5e45067a2a93e1e7afaa3a8c278d642bf6
RadomirNaidenov <nairado2007@gmail.com>
2024-08-01 17:24:35 +0300

    Update 02.Hero.py

--- Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Exercises/02.Hero.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Exercises/02.Hero.py
@@ -15,11 +15,6 @@ class Hero:
     def heal(self, amount,):
         self.health += amount
 
-hero = Hero("Peter", 100)
-print(hero.defend(50))
-hero.heal(50)
-print(hero.defend(99))
-print(hero.defend(1))
 
 
 
