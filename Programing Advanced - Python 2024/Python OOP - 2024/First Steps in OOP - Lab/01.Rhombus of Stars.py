f39e7fbe229582bb0993b1ec8bd19119710bcd61
RadomirNaidenov <nairado2007@gmail.com>
2024-08-01 16:57:47 +0300

    Create 01.Rhombus of Stars.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Lab/01.Rhombus of Stars.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+def print_rows(size, row):
+    print(f'{" " * (size - row)}{"* " * row}')
+    
+def print_upper_part(size):
+    for row in range(1, size + 1):
+        print_rows(size, row)
+
+def print_bottom_part(size):
+    for row in range(size - 1, 0, -1):
+        print_rows(size, row)
+
+def print_rhombus(size):
+    print_upper_part(size)
+    print_bottom_part(size)
+
+
+print_rhombus(n)
+
