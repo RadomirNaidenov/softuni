e232908a3a52185116589abb1d1d9718075e66a3
RadomirNaidenov <nairado2007@gmail.com>
2024-08-01 16:57:42 +0300

    Create 05.Music.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Lab/05.Music.py
@@ -0,0 +1,14 @@
+class Music:
+
+    def __init__(self, title, artist, lyrics) -> None:
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        return f'This is "{self.title}" from "{self.artist}"'
+    
+    def play(self):
+        return self.lyrics
+
+
