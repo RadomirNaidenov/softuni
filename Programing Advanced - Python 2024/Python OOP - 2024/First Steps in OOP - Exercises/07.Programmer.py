abf41151fbe8bfa3cb5b4f55829dfe6f5451597d
RadomirNaidenov <nairado2007@gmail.com>
2024-08-02 01:00:33 +0300

    Create 07.Programmer.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Exercises/07.Programmer.py
@@ -0,0 +1,25 @@
+class Programmer:
+
+    def __init__(self, name, language, skills) -> None:
+        self.name = name
+        self.language = language
+        self.skills = skills
+    
+    def watch_course(self, course_name, language, skills_earned):
+        if self.language != language:
+            return f"{self.name} does not know {language}"
+        self.skills += skills_earned
+        return f"{self.name} watched {course_name}"
+    
+    def change_language(self, new_language, skills_needed):
+        if self.skills < skills_needed:
+             needed_skills = skills_needed - self.skills
+             return f"{self.name} needs {needed_skills} more skills"
+        elif self.skills >= skills_needed and new_language != self.language:
+             previous_language = self.language
+             self.language = new_language
+             return f"{self.name} switched from {previous_language} to {new_language}"
+        elif self.skills >= skills_needed and new_language == self.language:
+            return f"{self.name} already knows {new_language}"
+
+
