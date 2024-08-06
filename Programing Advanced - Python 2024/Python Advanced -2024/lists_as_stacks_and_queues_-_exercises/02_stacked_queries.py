aeff14c6efed08a1ab65d05ac543c2cafd16d5e0
RadomirNaidenov <nairado2007@gmail.com>
2024-08-07 02:17:16 +0300

    Create 02_stacked_queries.py

---
+++ Programing Advanced - Python 2024/Python Advanced -2024/lists_as_stacks_and_queues_-_exercises/02_stacked_queries.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+deque_with_numbers = []
+number_of_command = int(input())
+
+for _ in range(number_of_command):
+    queries = input().split()
+    action = queries[0]
+
+    if action == "1":
+        deque_with_numbers.append(int(queries[1]))
+    elif action == "2":
+        if deque_with_numbers:
+            deque_with_numbers.pop()
+    elif action == "3":
+        if deque_with_numbers:
+            print(max(deque_with_numbers))
+    elif action == "4":
+        if deque_with_numbers:  
+            print(min(deque_with_numbers))
+        
+
+
+print(", ".join(str(x) for x in deque_with_numbers[::-1]))
+
