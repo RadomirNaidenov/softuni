5a890d058f1c569ff43821890494cb178a66ce72
RadomirNaidenov <nairado2007@gmail.com>
2024-08-08 19:47:22 +0300

    solution

---
+++ Programing Advanced - Python 2024/Python Advanced -2024/lists_as_stacks_and_queues_-_exercises/10_cups_and_bottles.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+cups = deque(int(cup) for cup in input().split())
+bottles = deque(int(bottle) for bottle in input().split())
+
+wasted_water = 0
+
+while cups and bottles:
+    current_cup = cups.popleft()
+    current_bottle = bottles.pop()
+
+    if current_cup <= current_bottle:
+        wasted_water += current_bottle - current_cup
+        continue
+    cups.appendleft(current_cup - current_bottle)
+
+if cups:
+    print(f"Cups:", *cups)
+else:
+    print(f"Bottle:", *bottles)
+
+print(f"Wasted litters of water: {wasted_water}")
+
+
+    
+
+
+
+
+
+
+    
