7878bdc155ac7f71b6544b165910122f953bfc2a
RadomirNaidenov <nairado2007@gmail.com>
2024-08-13 03:19:31 +0300

    Update 03_periodic_table.py

--- Programing Advanced - Python 2024/Python Advanced -2024/tuples_and_sets_-_exercise/03_periodic_table.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/tuples_and_sets_-_exercise/03_periodic_table.py
@@ -4,8 +4,6 @@ for _ in range(n):
     elements = input().split()
 
     for element in elements:
-        if element in unique_elements:
-            continue
         unique_elements.add(element)
 
 for element in unique_elements:
