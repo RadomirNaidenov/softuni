8f191690b9ecbe841687baa05c1954610e0f48c3
RadomirNaidenov <nairado2007@gmail.com>
2024-08-13 03:22:24 +0300

    Update 02_stacked_queries.py

--- Programing Advanced - Python 2024/Python Advanced -2024/lists_as_stacks_and_queues_-_exercises/02_stacked_queries.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/lists_as_stacks_and_queues_-_exercises/02_stacked_queries.py
@@ -18,8 +18,6 @@ for _ in range(number_of_command):
     elif action == "4":
         if deque_with_numbers:  
             print(min(deque_with_numbers))
-        
-
 
 print(", ".join(str(x) for x in deque_with_numbers[::-1]))
 
