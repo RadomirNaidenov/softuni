a5152e888bb32376420dd918f2f4153e7e6e17bf
RadomirNaidenov <nairado2007@gmail.com>
2024-08-17 17:40:15 +0300

    Update 04_maximal_sum.py

--- Programing Advanced - Python 2024/Python Advanced -2024/Multidimensional Lists - Exercise 1/04_maximal_sum.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/Multidimensional Lists - Exercise 1/04_maximal_sum.py
@@ -4,37 +4,15 @@ import sys
 def check_sum(some_rows, some_columns, some_matrix):
     max_sum = -sys.maxsize
     sub_matrix = []
-    for row in range(rows - 2):
-        for col in range(columns - 2):
-            first_number_in_a_row = matrix[row][col]
-            second_number_in_a_row = matrix[row][col + 1]
-            third_number_in_a_row = matrix[row][col + 2]
+    for row in range(some_rows - 2):
+        for col in range(some_columns - 2):
 
-            first_number_in_a_second_row = matrix[row + 1][col]
-            second_number_in_a_second_row = matrix[row + 1][col + 1]
-            third_number_in_a_second_row = matrix[row + 1][col + 2]
+            square = [some_matrix[row][col:col + 3] for row in range(row, row + 3)]
 
-            first_number_in_a_third_row = matrix[row + 2][col]
-            second_number_in_a_third_row = matrix[row + 2][col + 1]
-            third_number_in_a_third_row = matrix[row + 2][col + 2]
-
-            current_sum = first_number_in_a_row + \
-                second_number_in_a_row + \
-                third_number_in_a_row + \
-                first_number_in_a_second_row + \
-                second_number_in_a_second_row + \
-                third_number_in_a_second_row + \
-                first_number_in_a_third_row + \
-                second_number_in_a_third_row + \
-                third_number_in_a_third_row
-
+            current_sum = sum([num for sub_lst in square for num in sub_lst])
             if current_sum > max_sum:
                 max_sum = current_sum
-                sub_matrix = [
-                    [first_number_in_a_row, second_number_in_a_row, third_number_in_a_row],
-                    [first_number_in_a_second_row, second_number_in_a_second_row, third_number_in_a_second_row],
-                    [first_number_in_a_third_row, second_number_in_a_third_row, third_number_in_a_third_row]
-                ]
+                sub_matrix = square
 
     return max_sum, sub_matrix
 
@@ -44,8 +22,8 @@ matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
 max_sum, sub_matrix = check_sum(rows, columns, matrix)
 print(f"Sum = {max_sum}")
-for row in sub_matrix:
-    print(" ".join(map(str, row)))
+for numbers in sub_matrix:
+    print(*numbers)
 
 
 
