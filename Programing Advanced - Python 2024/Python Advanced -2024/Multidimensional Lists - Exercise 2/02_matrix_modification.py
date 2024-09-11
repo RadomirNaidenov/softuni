5c366d23ae501f9ddc265310bb4e950af3936dc5
RadomirNaidenov <nairado2007@gmail.com>
2024-09-11 16:28:39 +0300

    Create 02_matrix_modification.py

---
+++ Programing Advanced - Python 2024/Python Advanced -2024/Multidimensional Lists - Exercise 2/02_matrix_modification.py
@@ -0,0 +1,47 @@
+def add_number(matrix: list, row: int, col: int, value: int) -> None:
+    matrix[row][col] += value
+    
+
+
+def sebtract_number(matrix: list, row: int, col: int, value: int) -> None:
+    matrix[row][col] -= value
+    
+
+
+def valid_cordinates(matrix, row= int, col= int) -> bool:
+    if 0 <= row < len(matrix) and 0 <= col < len(matrix):
+        return True
+    return False
+        
+
+rows = int(input())
+matrix = [[int(x) for x in input().split()] for row in range(rows)]
+
+command = input()
+while command != "END":
+    command = command.split()
+
+    if command[0] == "Add":
+        row = int(command[1])
+        col = int(command[2])
+        value = int(command[3])
+        if not valid_cordinates(matrix, row, col):
+            print("Invalid coordinates")
+            command = input()
+            continue
+        add_number(matrix, row, col, value)
+    elif command[0] == "Subtract":
+        row = int(command[1])
+        col = int(command[2])
+        value = int(command[3])
+        if not valid_cordinates(matrix, row, col):
+            print("Invalid coordinates")
+            command = input()
+            continue
+        sebtract_number(matrix, row, col, value)
+
+    command = input()
+
+for lst in matrix:
+    print(*lst)
+
