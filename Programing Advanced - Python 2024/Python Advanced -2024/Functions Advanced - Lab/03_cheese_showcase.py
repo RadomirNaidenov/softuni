3b0f93ef5ac1061efbd945ba1301af973b80ecd6
RadomirNaidenov <nairado2007@gmail.com>
2024-09-13 19:10:13 +0300

    Update 03_cheese_showcase.py

--- Programing Advanced - Python 2024/Python Advanced -2024/Functions Advanced - Lab/03_cheese_showcase.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/Functions Advanced - Lab/03_cheese_showcase.py
@@ -1,13 +1,10 @@
 def sorting_cheeses(**kwargs):
     sorted_cheeses = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
-    result = ""
+    result = []
     for cheeses_name, quantities in sorted_cheeses:
-        result += f"{cheeses_name}\n"
-        reversed_quanities = sorted(quantities, reverse=True)
-        for quantity in reversed_quanities:
-            result += f"{quantity}\n"
+        result.append(cheeses_name)
+        result.extend([str(quantity) for quantity in sorted(quantities, reverse=True)])
     
-    return result
-
+    return "\n".join(result)
 
 
