0d1da743fb3069dc8a77bbc8c3b285176afa95d1
RadomirNaidenov <nairado2007@gmail.com>
2024-09-28 13:33:55 +0300

    Update 03_wild_card_game.py

--- Programing Advanced - Python 2024/Python Advanced -2024/Exams/Exam - 3/03_wild_card_game.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/Exams/Exam - 3/03_wild_card_game.py
@@ -1,37 +1,27 @@
 def draw_cards(*args, **kwargs):
-    type_dragon_lst = []
-    type_spell_lst = []
+    cards = {}
 
-    for arg in args:
-        name_of_the_card = arg[0]
-        type_of_the_card = arg[1]
-        if type_of_the_card == "monster":
-            type_dragon_lst.append(name_of_the_card)
-        elif type_of_the_card == "spell":
-            type_spell_lst.append(name_of_the_card)
+    for card_name, card_type in args:
+        cards[card_type ] = cards.get(card_type , []) + [card_name]
     
     for dragon_name, type_dragon in kwargs.items():
-        if type_dragon == "spell":
-            type_spell_lst.append(dragon_name)
-        elif type_dragon == "monster":
-            type_dragon_lst.append(dragon_name)
-    
+        cards[type_dragon] = cards.get(type_dragon, []) + [dragon_name]
+
+    type_dragon_lst = cards.get("dragon", [])
+    type_spell_lst = cards.get("spell", [])
+
     sorted_type_dragon_lst = sorted(type_dragon_lst)
     sorted_type_spell_lst = sorted(type_spell_lst)
 
-    
     if type_dragon_lst:
         print("Monster cards:")
-        print()
         for dragon in sorted_type_dragon_lst:
             print(f"  ***{dragon}")
-            print()
+
     if type_spell_lst:
         print("Spell cards:")
-        print()
         for spell_name in sorted_type_spell_lst:
             print(f"  $$${spell_name}")
-            print()
 
 
 print(draw_cards(("brave attack", "spell"), ("freeze", "spell"), lightning_bolt="spell", fireball="spell",))
