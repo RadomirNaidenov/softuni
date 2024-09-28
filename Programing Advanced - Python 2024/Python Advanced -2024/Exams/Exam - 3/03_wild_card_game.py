6921a370c0b172d20e70829a1496e6a47148c5d4
RadomirNaidenov <nairado2007@gmail.com>
2024-09-28 13:34:32 +0300

    Update 03_wild_card_game.py

--- Programing Advanced - Python 2024/Python Advanced -2024/Exams/Exam - 3/03_wild_card_game.py
+++ Programing Advanced - Python 2024/Python Advanced -2024/Exams/Exam - 3/03_wild_card_game.py
@@ -1,27 +1,25 @@
 def draw_cards(*args, **kwargs):
+    print_mapper = {
+        "monster": {"print_symbol": "***", "print_msg": "Monster cards:", "sort_by": "desc"},
+        "spell": {"print_symbol": "$$$", "print_msg": "Spell cards:", "sort_by": "asc"}
+    }
     cards = {}
 
     for card_name, card_type in args:
-        cards[card_type ] = cards.get(card_type , []) + [card_name]
-    
-    for dragon_name, type_dragon in kwargs.items():
-        cards[type_dragon] = cards.get(type_dragon, []) + [dragon_name]
+        cards[card_type] = cards.get(card_type, []) + [card_name]
 
-    type_dragon_lst = cards.get("dragon", [])
-    type_spell_lst = cards.get("spell", [])
-
-    sorted_type_dragon_lst = sorted(type_dragon_lst)
-    sorted_type_spell_lst = sorted(type_spell_lst)
-
-    if type_dragon_lst:
-        print("Monster cards:")
-        for dragon in sorted_type_dragon_lst:
-            print(f"  ***{dragon}")
-
-    if type_spell_lst:
-        print("Spell cards:")
-        for spell_name in sorted_type_spell_lst:
-            print(f"  $$${spell_name}")
+    for card_name, card_type in kwargs.items():
+        cards[card_type] = cards.get(card_type, []) + [card_name]
 
 
-print(draw_cards(("brave attack", "spell"), ("freeze", "spell"), lightning_bolt="spell", fireball="spell",))
+    output = []
+    for name, collection in cards.items():
+        if not collection:
+            continue
+
+        sorted_collection = sorted(collection) if print_mapper[name]["sort_by"] == "asc" else sorted(collection, reverse=True)
+        output.append(print_mapper[name]["print_msg"])
+        for card in sorted_collection:
+            output.append(f"  {print_mapper[name]['print_symbol']}{card}")
+
+    return "\n".join(output)
