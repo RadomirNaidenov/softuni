a68c59677fddc3e82d6660d55dbb0baa78ec23e6
RadomirNaidenov <nairado2007@gmail.com>
2024-10-07 17:54:59 +0300

    Update trainer.py

--- Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Exercises/project/trainer.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/First Steps in OOP - Exercises/project/trainer.py
@@ -27,13 +27,4 @@ class Trainer:
         return '\n'.join(output)
 
 
-# pokemon = Pokemon("Pikachu", 90)
-# print(pokemon.pokemon_details())
-# trainer = Trainer("Ash")
-# print(trainer.add_pokemon(pokemon))
-# second_pokemon = Pokemon("Charizard", 110)
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.trainer_data())
+
