af130326d880a8120fb9d66373176aeee3914895
RadomirNaidenov <nairado2007@gmail.com>
2024-10-29 14:51:27 +0200

    Update vehicle.py

--- Programing Advanced - Python 2024/Python OOP - 2024/Inheritance - Exercise/04_need_for_speed/project/vehicle.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/Inheritance - Exercise/04_need_for_speed/project/vehicle.py
@@ -5,14 +5,11 @@ class Vehicle:
         self.fuel = fuel
         self.horse_power = horse_power
         self.fuel_consumption = Vehicle.DEFAULT_FUEL_CONSUMPTION
-        self.can_drive = True
 
     def drive(self, kilometers):
-        if self.can_drive:
-            if self.fuel - (kilometers * self.DEFAULT_FUEL_CONSUMPTION) >= 0:
-                self.fuel -= (kilometers * self.DEFAULT_FUEL_CONSUMPTION)
-                if self.fuel == 0:
-                    self.can_drive = False
+        if self.fuel >= (kilometers * self.DEFAULT_FUEL_CONSUMPTION):
+            self.fuel -= (kilometers * self.DEFAULT_FUEL_CONSUMPTION)
+
 
 
 
