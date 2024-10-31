63be750ff8539c0c2b37a31fb30847ac841bd4de
RadomirNaidenov <nairado2007@gmail.com>
2024-10-31 23:15:39 +0200

    Create pizza.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Encapsulation - Exercise/02_pizza_maker/project/pizza.py
@@ -0,0 +1,56 @@
+from project.topping import Topping
+from project.dough import Dough
+
+
+class Pizza:
+
+    def __init__(self, name: str, dough: Dough, max_number_of_toppings: int):
+        self.name = name
+        self.dough = dough
+        self.max_number_of_toppings = max_number_of_toppings
+        self.toppings = {}
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("The name cannot be an empty string")
+        self.__name = value
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        if value is None:
+            raise ValueError("You should add dough to the pizza")
+        self.__dough = value
+
+    @property
+    def max_number_of_toppings(self):
+        return self.__max_number_of_toppings
+
+    @max_number_of_toppings.setter
+    def max_number_of_toppings(self, value):
+        if value <= 0:
+            raise ValueError("The maximum number of toppings cannot be less or equal to zero")
+        self.__max_number_of_toppings = value
+
+    def add_topping(self, topping: Topping):
+        if self.max_number_of_toppings == len(self.toppings):
+            raise ValueError("Not enough space for another topping")
+        self.toppings[topping.topping_type] = self.toppings.get(topping.topping_type, 0) + topping.weight
+
+    def calculate_total_weight(self):
+        return self.dough.weight + sum(self.toppings.values())
+
+
+
+
+
+
+
