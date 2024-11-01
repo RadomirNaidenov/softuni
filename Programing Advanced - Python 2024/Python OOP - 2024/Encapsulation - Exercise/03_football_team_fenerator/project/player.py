a6a48bc521b9ef797bccaf01019581ee8e4d883d
RadomirNaidenov <nairado2007@gmail.com>
2024-11-01 17:31:27 +0200

    Create player.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Encapsulation - Exercise/03_football_team_fenerator/project/player.py
@@ -0,0 +1,25 @@
+class Player:
+
+    def __init__(self, name: str, sprint: int, dribble: int, passing: int, shooting: int):
+        self.__name = name
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    def __str__(self) -> str:
+        return (f"Player: {self.__name}\n"
+                f"Sprint: {self.__sprint}\n"
+                f"Dribble: {self.__dribble}\n"
+                f"Passing: {self.__passing}\n"
+                f"Shooting: {self.__shooting}")
+
+
