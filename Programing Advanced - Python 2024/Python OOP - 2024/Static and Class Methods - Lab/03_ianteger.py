23845c7674645aa16c014d4e60672935fa89c58a
RadomirNaidenov <nairado2007@gmail.com>
2024-11-02 02:01:57 +0200

    Create 03_ianteger.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Static and Class Methods - Lab/03_ianteger.py
@@ -0,0 +1,43 @@
+class Integer:
+
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value: float):
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(float_value)
+    
+    @classmethod
+    def from_roman(cls, value):
+        roman_numerals = {"I" : 1,
+                  "V" : 5,
+                  "X" : 10,
+                  "L" : 50,
+                  "C" : 100,
+                  "D" : 500,
+                  "M" : 1000
+                  }
+
+        int_value = 0
+
+        for i in range(len(value)):
+            if value[i] in roman_numerals:
+                    if i + 1 < len(value) and roman_numerals[value[i]] < roman_numerals[value[i + 1]]:
+                        int_value -= roman_numerals[value[i]]
+                    else:
+                        int_value += roman_numerals[value[i]]
+
+        return cls(int_value)
+    
+    @classmethod
+    def from_string(cls, value):
+        if str(value).isdigit():
+            return cls(int(value))
+        return "wrong type"
+        
+Integer(1)
+integer = Integer.from_float(2.5)
+Integer.from_float("2.5")
+        
