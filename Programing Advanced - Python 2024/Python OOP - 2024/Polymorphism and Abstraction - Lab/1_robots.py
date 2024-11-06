c4fb622436f74ee3d0c2e87068e4ddec759b2395
RadomirNaidenov <nairado2007@gmail.com>
2024-11-06 23:44:08 +0200

    Create 1_robots.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Polymorphism and Abstraction - Lab/1_robots.py
@@ -0,0 +1,28 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def sensors_amount():
+        return 1
+
+
+class MedicalRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 6
+
+
+class ChefRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 4
+
+
+class WarRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 12
+
+
+
