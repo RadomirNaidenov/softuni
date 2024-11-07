97809d753f2ac3f25098ed95a1b865e834901458
RadomirNaidenov <nairado2007@gmail.com>
2024-11-07 22:48:13 +0200

    Create 02_groups.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Polymorphism and Abstraction - Exercise/02_groups.py
@@ -0,0 +1,34 @@
+class Person:
+
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+
+    def __repr__(self) -> str:
+        # return self.name + " " + self.username
+        return f"{self.name} {self.surname}"
+
+    def __add__(self, other):
+        if isinstance(other, Person):
+            return Person(self.name, other.surname)
+
+
+class Group:
+
+    def __init__(self, name: str, people: list[Person]):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}", self.people + other.people)
+
+    def __repr__(self):
+        return f"Group {self.name} with members {', '.join(str(x) for x in self.people)}"
+
+    def __getitem__(self, item):
+        return f"Person {item}: {self.people[item]}"
+
+
