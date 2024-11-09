257d1585da9bf1e1361b5198132db1ff1f570705
RadomirNaidenov <nairado2007@gmail.com>
2024-11-09 13:11:52 +0200

    Create mercedes_team.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Polymorphism and Abstraction - Exercise/06_formula_1_manager/project/formula_teams/mercedes_team.py
@@ -0,0 +1,21 @@
+from project.formula_teams.formula_team import FormulaTeam
+
+
+class MercedesTeam(FormulaTeam):
+
+    @property
+    def sponsors(self) -> dict[str: dict[int: int]]:
+        return {
+            "Petronas": {
+                1: 1_000_000,
+                3: 500_000
+            },
+            "TeamViewer": {
+                5: 100_000,
+                7: 50_000
+            }
+        }
+
+    @property
+    def expenses(self):
+        return 200_000
