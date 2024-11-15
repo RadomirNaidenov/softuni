e428b265d21c2f58c11e8341f9d8cb9f638ba0c9
RadomirNaidenov <nairado2007@gmail.com>
2024-11-15 20:02:01 +0200

    Update 05_cache.py

--- Programing Advanced - Python 2024/Python OOP - 2024/Decorators - Exercise/05_cache.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/Decorators - Exercise/05_cache.py
@@ -1,1 +1,12 @@
+def cache(func):
+
+    def wrapper(number):
+
+        if number not in wrapper.log:
+            wrapper.log[number] = func(number)
+
+        return wrapper.log[number]
+
+    wrapper.log = {}
+    return wrapper
 
