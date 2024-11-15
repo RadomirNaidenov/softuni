1b41c5717764ac013c4099392f841db620530f5e
RadomirNaidenov <nairado2007@gmail.com>
2024-11-15 20:02:24 +0200

    Create computer_store_app.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Decorators - Exercise/09_computer_store/project/computer_store_app.py
@@ -0,0 +1,38 @@
+from typing import List
+from project.computer_types.computer import Computer
+from project.computer_types.laptop import Laptop
+
+
+class ComputerStoreApp:
+
+    VALID_COMPUTERS = {
+        "Desktop Computer": Computer,
+        "Laptop": Laptop
+    }
+
+    def __init__(self):
+        self.warehouse: List[Computer] = []
+        self.profits: int = 0
+
+    def build_computer(self, type_computer: str, manufacturer: str, model: str, processor: str, ram: int):
+        try:
+            computer = self.VALID_COMPUTERS[type_computer](manufacturer, model)
+        except KeyError:
+            raise ValueError(f"{type_computer} is not a valid type computer!")
+
+        configuration = computer.configure_computer(processor, ram)
+        self.warehouse.append(computer)
+
+        return configuration
+
+    def sell_computer(self, client_budget: int, wanted_processor: str, wanted_ram: int):
+        for computer in self.warehouse:
+            if computer.price <= client_budget and \
+                    computer.processor == wanted_processor and \
+                    computer.ram >= wanted_ram:
+                self.profits += client_budget - computer.price
+                self.warehouse.remove(computer)
+
+                return f"{computer} sold for {client_budget}$."
+
+        raise Exception("Sorry, we don't have a computer for you.")
