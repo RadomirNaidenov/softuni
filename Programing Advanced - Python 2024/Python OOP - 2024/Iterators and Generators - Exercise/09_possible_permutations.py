85bc587d3dd02c9deeea0097203a5efacc80b413
RadomirNaidenov <nairado2007@gmail.com>
2024-11-17 17:35:17 +0200

    Update 09_possible_permutations.py

--- Programing Advanced - Python 2024/Python OOP - 2024/Iterators and Generators - Exercise/09_possible_permutations.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/Iterators and Generators - Exercise/09_possible_permutations.py
@@ -1,9 +1,9 @@
 from itertools import permutations
 
-def possible_permutations(some_list: list):
-        for el in permutations(some_list):
-            yield list(el)
 
+def possible_permutations(some_list: list):
+    for el in permutations(some_list):
+        yield list(el)
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
