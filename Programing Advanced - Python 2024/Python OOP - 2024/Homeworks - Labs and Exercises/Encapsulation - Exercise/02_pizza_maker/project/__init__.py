5f8f5390560b1f579959edfeb946c8a43288bcb9
RadomirNaidenov <nairado2007@gmail.com>
2024-11-23 10:38:53 +0200

    Update __init__.py

--- Programing Advanced - Python 2024/Python OOP - 2024/Homeworks - Labs and Exercises/Encapsulation - Exercise/02_pizza_maker/project/__init__.py
+++ Programing Advanced - Python 2024/Python OOP - 2024/Homeworks - Labs and Exercises/Encapsulation - Exercise/02_pizza_maker/project/__init__.py
@@ -1,6 +1,6 @@
-# from project.dough import Dough
-# from project.pizza import Pizza
-# from project.topping import Topping
+# from project1_2.dough import Dough
+# from project1_2.pizza import Pizza
+# from project1_2.topping import Topping
 #
 #
 # tomato_topping = Topping("Tomato", 60)
