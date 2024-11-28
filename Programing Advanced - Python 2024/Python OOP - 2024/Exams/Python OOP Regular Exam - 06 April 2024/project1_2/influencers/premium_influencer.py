4c183fb0ab567149c92b17783adee9e77c9faad4
RadomirNaidenov <nairado2007@gmail.com>
2024-11-28 18:05:06 +0200

    Create premium_influencer.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Exams/Python OOP Regular Exam - 06 April 2024/project1_2/influencers/premium_influencer.py
@@ -0,0 +1,21 @@
+from project3.influencers.base_influencer import BaseInfluencer
+from project3.campaigns.base_campaign import BaseCampaign
+
+
+class PremiumInfluencer(BaseInfluencer):
+    initial_payment_percentage = 0.85
+
+    def __init__(self, username: str, followers: int, engagement_rate: float):
+        super().__init__(username, followers, engagement_rate)
+
+    def calculate_payment(self, campaign: BaseCampaign) -> float:
+        return campaign.budget * self.initial_payment_percentage
+
+    def reached_followers(self, campaign_type: str) -> int:
+        if campaign_type == "HighBudgetCampaign":
+            return int(self.followers * self.engagement_rate * 1.5)
+        elif campaign_type == "LowBudgetCampaign":
+            return int(self.followers * self.engagement_rate * 0.8)
+
+
+
