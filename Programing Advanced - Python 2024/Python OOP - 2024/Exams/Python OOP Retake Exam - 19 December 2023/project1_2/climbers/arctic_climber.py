a6078d42142435bd017c1afc17203e89f3c6c4be
RadomirNaidenov <nairado2007@gmail.com>
2024-11-30 12:58:29 +0200

    Create arctic_climber.py

---
+++ Programing Advanced - Python 2024/Python OOP - 2024/Exams/Python OOP Retake Exam - 19 December 2023/project1_2/climbers/arctic_climber.py
@@ -0,0 +1,16 @@
+from project1_2.climbers.base_climber import BaseClimber
+from project1_2.peaks.base_peak import BasePeak
+
+
+class ArcticClimber(BaseClimber):
+    def __init__(self, name: str):
+        super().__init__(name, 200)
+
+    def can_climb(self):
+        return self.strength >= 100
+
+    def climb(self, peak: BasePeak):
+        difficulty_multiplier = 2.0 if peak.difficulty_level == "Extreme" else 1.5
+        self.strength -= 20 * difficulty_multiplier
+        self.conquered_peaks.append(peak.name)
+
