632db04b6f1175dd91c47b64ad1800b7fcb0376a
RadomirNaidenov <nairado2007@gmail.com>
2025-03-29 18:05:15 +0200

    made empty file

--- Programing Basics - Python/empty.py
+++ Programing Basics - Python/empty.py
@@ -1,28 +1,4 @@
-painted_eggs = {
-    "red": 0,
-    "orange": 0,
-    "blue": 0,
-    "green": 0
-}
 
-for _ in range(int(input())):
-    egg_color = input()
-    painted_eggs[egg_color] += 1
-
-mag_eggs_color = max(painted_eggs, key=lambda k: painted_eggs[k])
-max_eggs_color_count = painted_eggs[mag_eggs_color]
-
-result = []
-
-for paint, count in painted_eggs.items():
-    paint = paint[:1].upper() + paint[1:]
-    print(f"{paint} eggs: {count}")
-
-
-mag_eggs_color = max(painted_eggs, key=lambda k: painted_eggs[k])
-max_eggs_color_count = painted_eggs[mag_eggs_color]
-
-print(f"Max eggs: {max_eggs_color_count} -> {mag_eggs_color}")
 
 
 
