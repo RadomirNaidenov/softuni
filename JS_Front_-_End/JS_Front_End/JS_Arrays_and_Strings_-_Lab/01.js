797416e32c164df3974c63d784b9c5d83aecef27
RadomirNaidenov <nairado2007@gmail.com>
2025-06-28 17:33:44 +0300

    solutions

---
+++ JS_Front_-_End/JS_Front_End/JS_Arrays_and_Strings_-_Lab/01.js
@@ -0,0 +1,15 @@
+function sumNums(array) {
+    // let firstNum = array.shift();
+    // let lastNum = array.pop();
+
+    // let firstNum = array.at(0);
+    // let lastNum = array.at(-1);
+
+    let firstNum = array[0];
+    let lastNum = array[array.length - 1];
+
+    console.log(firstNum + lastNum);
+    
+}
+
+sumNums([20, 30, 40])
